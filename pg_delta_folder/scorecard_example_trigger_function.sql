8a102c2de600e5a152fc63b92a5dcd0eeee4e913
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-09-12 19:49:02 +0530

    Update scorecard_example_trigger_function.sql

--- pg_delta_folder/scorecard_example_trigger_function.sql
+++ pg_delta_folder/scorecard_example_trigger_function.sql
@@ -10,6 +10,7 @@ DECLARE
   column_value TEXT;
   payload_items TEXT[];
   uniquecolumn TEXT;
+  logtime TEXT;
 BEGIN
   -- Set record row depending on operation
   CASE TG_OP
@@ -26,8 +27,30 @@ BEGIN
     EXECUTE format('SELECT $1.%I::TEXT', column_name)
     INTO column_value
     USING rec;
+   case 
+    when 
+    column_name = 'upload_document' then 
+         --  RAISE NOTICE 'upload_document boolean';
+         if column_value = 'false' then
+                column_value = '0';
+        else
+                column_value = '1';
+        end if;
+    when
+    column_name = 'upload_document_required' then
+         -- RAISE NOTICE 'upload_document_required boolean';
+        if column_value = 'false' then
+                column_value = '0';
+        else
+                column_value = '1';     
+        end if;
+    else
+    -- RAISE NOTICE ' not boolean';
+    end case;
     payload_items := array_append(payload_items, '"' || replace(column_name, '"', '\"') || '":"' || replace(column_value, '"', '\"') || '"');
   END LOOP;
+  --logtime := (select date_display_tz());
+  logtime := (SELECT to_char (now()::timestamptz at time zone 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS"Z"'));
   
   uniquecolumn := (SELECT c.column_name
         FROM information_schema.key_column_usage AS c
@@ -40,13 +63,17 @@ BEGIN
               || '{'
               || '"topic":"' || 'db.postgres.sync' || '",'
               || '"originator":"' || 'tc-postgres-delta-processor' || '",'
-              || '"timestamp":"' || CURRENT_TIMESTAMP                    || '",'
+          --    || '"timestamp":"' || '2019-08-19T08:39:48.959Z'                   || '",'   
+            || '"timestamp":"' || logtime  || '",'
+              || '"mime-type":"' || 'application/json'                   || '",'
+              || '"payload": {'      
+              
               || '"Uniquecolumn":"' || uniquecolumn                   || '",'
               || '"operation":"' || TG_OP                                || '",'
               || '"schema":"'    || TG_TABLE_SCHEMA                      || '",'
               || '"table":"'     || TG_TABLE_NAME                        || '",'
-              || '"data":{'      || array_to_string(payload_items, ',')  || '}'
-              || '}';
+              || '"data": {'      || array_to_string(payload_items, ',')  || '}'
+              || '}}';
 
   -- Notify the channel
   PERFORM pg_notify('db_notifications', payload);
@@ -54,9 +81,47 @@ BEGIN
   RETURN rec;
 END;
 $body$ LANGUAGE plpgsql;
+                              
+
 
 CREATE TRIGGER "scorecard_trigger"
   AFTER INSERT OR DELETE OR UPDATE ON scorecard
   FOR EACH ROW
 EXECUTE PROCEDURE notify_trigger('scorecard_id', 'scorecard_status_id', 'scorecard_type_id', 'project_category_id', 'name', 'version', 'min_score', 'max_score', 'create_user', 'create_date', 'modify_user', 'modify_date', 'version_number');
 
+CREATE TRIGGER "default_scorecard_trigger"
+  AFTER INSERT OR DELETE OR UPDATE ON default_scorecard
+  FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger('project_category_id','scorecard_type_id', 'scorecard_id','create_user', 'create_date', 'modify_user', 'modify_date');
+
+CREATE TRIGGER "scorecard_group_trigger"
+  AFTER INSERT OR DELETE OR UPDATE ON scorecard_group
+  FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger('scorecard_group_id','scorecard_id','name','weight','sort','create_user','create_date','modify_user' ,'modify_date','version');
+
+CREATE TRIGGER "scorecard_question_trigger"
+  AFTER INSERT OR DELETE OR UPDATE ON scorecard_question
+  FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger('scorecard_question_id','scorecard_question_type_id','scorecard_section_id','description','guideline', 'weight','sort','upload_document','upload_document_required','create_user','create_date','modify_user' ,'modify_date','version');
+
+
+CREATE TRIGGER "scorecard_section_trigger"
+  AFTER INSERT OR DELETE OR UPDATE ON scorecard_section
+  FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger('scorecard_section_id','scorecard_group_id','name','weight','sort','create_user','create_date','modify_user' ,'modify_date','version');
+
+CREATE TRIGGER "scorecard_question_type_lu_trigger"
+  AFTER INSERT OR DELETE OR UPDATE ON scorecard_question_type_lu
+  FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger('scorecard_question_type_id', 'name', 'description', 'create_user', 'create_date', 'modify_user', 'modify_date', 'version');
+                              
+
+CREATE TRIGGER "scorecard_status_lu_trigger"
+  AFTER INSERT OR DELETE OR UPDATE ON scorecard_status_lu
+  FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger('scorecard_status_id', 'name', 'description', 'create_user', 'create_date', 'modify_user', 'modify_date','version');
+
+CREATE TRIGGER "scorecard_type_lu_trigger"
+  AFTER INSERT OR DELETE OR UPDATE ON scorecard_type_lu
+  FOR EACH ROW
+EXECUTE PROCEDURE notify_trigger('scorecard_type_id', 'name', 'description', 'create_user', 'create_date', 'modify_user', 'modify_date','version');
